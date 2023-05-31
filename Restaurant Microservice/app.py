26f9f63e565e12f869f958661c2ce1fe1a66df4b
Bishop-Y <66753948+Bishop-Y@users.noreply.github.com>
2023-05-31 22:37:45 +0300

    Добавил комментарии

--- Restaurant Microservice/app.py
+++ Restaurant Microservice/app.py
@@ -50,44 +50,53 @@ class OrderDish(db.Model):
     price = db.Column(db.Float, nullable=False)
 
 
+# Этот метод обрабатывает POST-запрос для создания нового заказа
 @app.route('/order', methods=['POST'])
 @jwt_required()
 def create_order():
-    user_id = get_jwt_identity()
-    data = request.get_json()
-    dish_list = data.get('dishes')
-    special_requests = data.get('special_requests')
+    user_id = get_jwt_identity()  # Получение уникального идентификатора пользователя из JWT
+    data = request.get_json()  # Получение данных из JSON запроса
+    dish_list = data.get('dishes')  # Получение списка блюд из данных
+    special_requests = data.get('special_requests')  # Получение специальных требований из данных
+    # Проверка валидности списка блюд
     if not dish_list or not isinstance(dish_list, list):
         return {"message": "Invalid data"}, 400
 
+    # Создание нового заказа с user_id и специальными требованиями
     order = Order(user_id=user_id, special_requests=special_requests)
     db.session.add(order)
     db.session.commit()
+    # Проходим по всем блюдам в списке и добавляем их в заказ
     for dish in dish_list:
         dish_id = dish.get('id')
         quantity = dish.get('quantity')
         dish_obj = Dish.query.get(dish_id)
+        # Проверка доступности блюда и достаточного количества
         if not dish_obj or dish_obj.quantity < quantity:
             return {"message": "Dish not available or quantity exceeded"}, 400
         else:
+            # Уменьшаем количество блюда на складе
             dish_obj.quantity -= quantity
+            # Создаем запись о заказе и блюде в таблице OrderDish
             order_dish = OrderDish(order_id=order.id, dish_id=dish_id, quantity=quantity, price=dish_obj.price)
             db.session.add(order_dish)
-
     db.session.commit()
+    # Возвращаем сообщение об успешном создании заказа и его ID
     return {
         "message": "Order Created",
         "order_id": order.id
     }, 201
 
 
+# Этот метод обрабатывает GET-запрос для получения информации о конкретном заказе по его ID
 @app.route('/order/<int:order_id>', methods=['GET'])
 @jwt_required()
 def get_order_by_id(order_id):
-    print(order_id)
-    order = Order.query.get(order_id)
+    order = Order.query.get(order_id)  # Получаем заказ по его ID
+    # Если заказ не найден, возвращаем сообщение об ошибке
     if not order:
         return {"message": "Order not found"}, 404
+    # Если заказ найден, возвращаем его ID и статус
     else:
         return {
             "order_id": order.id,
@@ -95,24 +104,28 @@ def get_order_by_id(order_id):
         }, 200
 
 
+
+# Этот метод вызывается для получения информации о пользователе по его токену
 def get_user_info(token):
     headers = {
         "Authorization": f"Bearer {token}"
     }
     response = requests.get("http://localhost:6700/auth/user", headers=headers)
-
+    # Если получить информацию о пользователе не удалось, возвращаем сообщение об ошибке
     if response.status_code != 200:
         return {'message': 'Failed to get user info'}, 404
+    # Если информация получена успешно, возвращаем ее
     else:
         return response.json()
 
-
+# Этот метод обрабатывает различные HTTP-запросы для управления блюдами (создание, просмотр, обновление и удаление)
 @app.route('/dish', methods=['POST', 'GET', 'PUT', 'DELETE'])
 @jwt_required()
 def manage_dish():
     token = request.headers.get('Authorization')
     token = token[7:]
     data = get_user_info(token)
+    # Если роль пользователя не 'manager', возвращаем полученные данные
     if data.get('role') != 'manager':
         return data
     print(request.method)
@@ -174,11 +187,14 @@ def manage_dish():
         }, 200
 
 
+# Этот метод обрабатывает GET-запрос для получения списка доступных блюд
 @app.route('/menu', methods=['GET'])
 def get_menu():
+    # Получаем все доступные блюда из базы данных
     available_dishes = Dish.query.filter(Dish.is_available == True).all()
 
     menu = []
+    # Создаем список из информации о доступных блюдах
     for dish in available_dishes:
         menu.append({
             "name": dish.name,
@@ -187,10 +203,13 @@ def get_menu():
             "quantity": dish.quantity
         })
 
+    # Если нет доступных блюд, возвращаем сообщение об ошибке
     if len(menu) == 0:
         return {"message": "No dishes available at the moment"}, 404
 
+    # Возвращаем список доступных блюд
     return {"menu": menu}, 200
+
 
 
 # Точка входа в приложение
